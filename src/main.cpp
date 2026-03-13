b542d283ff490d43248bb40bae281362a857f584
parkingrlbr-prog <parkingrlbr@gmail.com>
2026-03-13 21:38:21 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,40 @@
 #include <gdiplus.h>
 #include <filesystem>
 #include <string>
-#include <chrono>
 #include <sstream>
 #include <iomanip>
+#include <chrono>
 
 #pragma comment(lib,"gdiplus.lib")
 
 using namespace Gdiplus;
 
+// ------------------------ GLOBAL VARIABLES ------------------------
+HWND hwndOverlay = nullptr;
 POINT startPt, endPt;
 bool selecting = false;
-HWND hwndOverlay;
-
 ULONG_PTR gdiplusToken;
 
-// Funkcija za dobijanje CLSID PNG encoder-a
+// ------------------------ PNG HELPER ------------------------
 int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
 {
-    UINT num = 0;
-    UINT size = 0;
+    UINT num = 0, size = 0;
     GetImageEncodersSize(&num, &size);
     if (size == 0) return -1;
-
-    ImageCodecInfo* pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+    ImageCodecInfo* pImageCodecInfo = (ImageCodecInfo*)malloc(size);
     GetImageEncoders(num, size, pImageCodecInfo);
-
     for (UINT j = 0; j < num; ++j)
-    {
         if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0)
         {
             *pClsid = pImageCodecInfo[j].Clsid;
             free(pImageCodecInfo);
             return j;
         }
-    }
     free(pImageCodecInfo);
     return -1;
 }
 
-// Funkcija za screenshot
+// ------------------------ SCREENSHOT FUNCTION ------------------------
 void SaveScreenshot(int x, int y, int w, int h)
 {
     HDC hScreen = GetDC(NULL);
@@ -55,25 +50,24 @@ void SaveScreenshot(int x, int y, int w, int h)
     CLSID pngClsid;
     GetEncoderClsid(L"image/png", &pngClsid);
 
-    // Naziv fajla: screenshots/screen_YYYYMMDD_HHMMSS.png
+    // Naziv: screenshots/screen_YYYYMMDD_HHMMSS.png
     auto t = std::chrono::system_clock::now();
     std::time_t tt = std::chrono::system_clock::to_time_t(t);
     std::tm tm;
     localtime_s(&tm, &tt);
-    std::stringstream ss;
-    ss << "screenshots/screen_"
-       << std::put_time(&tm, "%Y%m%d_%H%M%S") << ".png";
-    std::wstring filename(ss.str().begin(), ss.str().end());
+    std::wstringstream ss;
+    ss << L"screenshots/screen_"
+       << std::put_time(&tm, L"%Y%m%d_%H%M%S") << L".png";
 
-    bmp.Save(filename.c_str(), &pngClsid, NULL);
+    bmp.Save(ss.str().c_str(), &pngClsid, NULL);
 
     DeleteObject(hBitmap);
     DeleteDC(hDC);
     ReleaseDC(NULL, hScreen);
 }
 
-// Overlay Window procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+// ------------------------ OVERLAY WINDOW PROCEDURE ------------------------
+LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
@@ -108,6 +102,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             int h = abs(startPt.y - endPt.y);
 
             SaveScreenshot(x, y, w, h);
+            ShowWindow(hwnd, SW_HIDE); // Sakrij overlay nakon snimanja
         }
         break;
 
@@ -117,9 +112,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         HDC hdc = BeginPaint(hwnd, &ps);
         if (selecting)
         {
-            // Prozirni overlay + pravougaonik
             HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 255));
-            SetBkMode(hdc, TRANSPARENT);
             FrameRect(hdc, &RECT{ startPt.x,startPt.y,endPt.x,endPt.y }, hBrush);
             DeleteObject(hBrush);
         }
@@ -137,6 +130,39 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+// ------------------------ CONTROL PANEL WINDOW ------------------------
+LRESULT CALLBACK ControlWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case 1: // Snip Region
+        {
+            ShowWindow(hwndOverlay, SW_SHOW);
+        }
+        break;
+        case 2: // Full Screen
+        {
+            int w = GetSystemMetrics(SM_CXSCREEN);
+            int h = GetSystemMetrics(SM_CYSCREEN);
+            SaveScreenshot(0, 0, w, h);
+        }
+        break;
+        case 3: // Exit
+            PostQuitMessage(0);
+            break;
+        }
+        break;
+
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+// ------------------------ MAIN ------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
     GdiplusStartupInput gdiplusStartupInput;
@@ -145,35 +171,61 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
     // Registracija hotkey *
     RegisterHotKey(NULL, 1, 0, VK_MULTIPLY);
 
-    WNDCLASS wc = { 0 };
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "SnipOverlay";
-
-    RegisterClass(&wc);
+    // Registracija overlay prozora
+    WNDCLASS wcOverlay = { 0 };
+    wcOverlay.lpfnWndProc = OverlayWndProc;
+    wcOverlay.hInstance = hInstance;
+    wcOverlay.lpszClassName = "SnipOverlay";
+    RegisterClass(&wcOverlay);
 
     hwndOverlay = CreateWindowEx(
         WS_EX_TOPMOST | WS_EX_LAYERED,
         "SnipOverlay",
-        "Snipping Tool",
+        "",
         WS_POPUP,
         0, 0,
         GetSystemMetrics(SM_CXSCREEN),
         GetSystemMetrics(SM_CYSCREEN),
         NULL, NULL, hInstance, NULL
     );
-
     SetLayeredWindowAttributes(hwndOverlay, 0, 120, LWA_ALPHA);
-    ShowWindow(hwndOverlay, SW_SHOW);
+    ShowWindow(hwndOverlay, SW_HIDE); // Sakrij overlay dok se ne klikne Snip Region
+
+    // Registracija kontrolnog prozora
+    WNDCLASS wcControl = { 0 };
+    wcControl.lpfnWndProc = ControlWndProc;
+    wcControl.hInstance = hInstance;
+    wcControl.lpszClassName = "ControlWindow";
+    RegisterClass(&wcControl);
+
+    HWND hwndControl = CreateWindow(
+        "ControlWindow",
+        "Snipping Tool Control",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        250, 120,
+        NULL, NULL, hInstance, NULL
+    );
+
+    // Dugmad
+    CreateWindow("BUTTON", "Snip Region", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+        20, 20, 100, 30, hwndControl, (HMENU)1, hInstance, NULL);
+    CreateWindow("BUTTON", "Full Screen", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+        130, 20, 100, 30, hwndControl, (HMENU)2, hInstance, NULL);
+    CreateWindow("BUTTON", "Exit", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+        75, 60, 100, 30, hwndControl, (HMENU)3, hInstance, NULL);
+
+    ShowWindow(hwndControl, SW_SHOW);
+    UpdateWindow(hwndControl);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
-        if (msg.message == WM_HOTKEY)
+        if (msg.message == WM_HOTKEY && msg.wParam == 1)
         {
             int w = GetSystemMetrics(SM_CXSCREEN);
             int h = GetSystemMetrics(SM_CYSCREEN);
-            SaveScreenshot(0, 0, w, h); // cijeli ekran
+            SaveScreenshot(0, 0, w, h);
         }
 
         TranslateMessage(&msg);
